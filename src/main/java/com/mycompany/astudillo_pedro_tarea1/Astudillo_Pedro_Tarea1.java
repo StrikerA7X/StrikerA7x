478a17448eb2d2899671560fe69c0feb95cfeb1c
TUF GAMING <TUF GAMING@LAPTOP-E7M3AB9F.mshome.net>
2022-03-31 21:42:49 -0500

    Tarea 1

---
+++ src/main/java/com/mycompany/astudillo_pedro_tarea1/Astudillo_Pedro_Tarea1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.astudillo_pedro_tarea1;
+
+/**
+ *
+ * @author TUF GAMING
+ */
+public class Astudillo_Pedro_Tarea1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
